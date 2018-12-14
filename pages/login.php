36b638e98e85c95a19dcf680137dc11e22c3a0f3
mrtwizard <b.m78990@gmail.com>
2018-12-14 12:46:08 +0100

    change html login && register pages

--- pages/login.php
+++ pages/login.php
@@ -1,21 +1,27 @@
 <?php 
+$page = 'Login';
 require '../includes/header.php';
+require '../includes/navbar.php';
 echo 'connexion <br>';
 ?>
 <form action="/zeblogphp/includes/login_script.php" method="POST">
   <div>
     <label for="mail">Mail:</label>
-    <input type="mail" name="mail" id="mail" placeholder="votre mail" required>
+    <input type="mail" name="mail" id="mail" placeholder="votre mail" required autocomplete="off">
   </div>
   <div>
     <label for="passwd">mot de passe:</label>
-    <input type="password" name="passwd" id="passwd" placeholder="votre mot de passe" required>
+    <input type="password" name="passwd" id="passwd" placeholder="votre mot de passe" required autocomplete="off">
   </div>
   <div>
     <input type="submit" id="submitbtn" name="submitbtn" value="Connexion">
   </div>
+  <div>
+  </div>
 </form>
-
+<div>
+  <a href="<?= "{$urls['register']}" ?>">Pas encore de compte? Inscrivez-vous!</a>
+</div>
 <?php
 require '../includes/footer.php';
 ?>
--- pages/register.php
+++ pages/register.php
@@ -1,38 +1,42 @@
 <?php
   require '../includes/header.php';
+  require '../includes/navbar.php';
 ?>
 <form action="/zeblogphp/includes/register_script.php" method="POST">
   <div>
     <label for="lastname">Nom: </label>
-    <input type="text" autocomplete="off" id="lastname" name="lastname" placeholder="Votre nom" required value="martin">
+    <input type="text" autocomplete="off" id="lastname" name="lastname" placeholder="Votre nom" required>
   </div>
   <div>
     <label for="firstname">Prénom: </label>
-    <input type="text" autocomplete="off" id="firstname" name="firstname" placeholder="Votre prénom" required value="baptiste">
+    <input type="text" autocomplete="off" id="firstname" name="firstname" placeholder="Votre prénom" required>
   </div>
   <div>
     <label for="pseudo">Pseudo: </label>
-    <input type="pseudo" autocomplete="off" id="pseudo" name="pseudo" placeholder="Votre pseudo" required value="bamartin">
+    <input type="pseudo" autocomplete="off" id="pseudo" name="pseudo" placeholder="Votre pseudo" required>
   </div>
   <div>
     <label for="mail">mail: </label>
-    <input type="mail" autocomplete="off" id="mail" name="mail" placeholder="Votre mail" required value="mail@mail.com">
+    <input type="mail" autocomplete="off" id="mail" name="mail" placeholder="Votre mail" required>
   </div>
   <div>
     <label for="cmail">Confirmation mail: </label>
-    <input type="mail" autocomplete="off" id="cmail" name="cmail" placeholder="Confirmez votre mail" required value="mail@mail.com">
+    <input type="mail" autocomplete="off" id="cmail" name="cmail" placeholder="Confirmez votre mail" required>
   </div>
   <div>
     <label for="passwd">Mot de passe: </label>
-    <input type="password" autocomplete="off" id="passwd" name="passwd" placeholder="Votre mot de passe" required value="mdp">
+    <input type="password" autocomplete="off" id="passwd" name="passwd" placeholder="Votre mot de passe" required>
   </div>
   <div>
     <label for="cpasswd">Confirmation mot de passe: </label>
-    <input type="password" autocomplete="off" id="cpasswd" name="cpasswd" placeholder="Confirmez votre mot de passe" required value="mdp">
+    <input type="password" autocomplete="off" id="cpasswd" name="cpasswd" placeholder="Confirmez votre mot de passe" required>
   </div>
   <div>
     <input type="submit" id="submitbtn" name="submitbtn" value="S'inscrire">
   </div>
 </form>
+<div>
+  <a href="<?= "{$urls['login']}" ?>">Deja un compte? connectez-vous!</a>
+</div>
 <?php
   require "../includes/footer.php";
