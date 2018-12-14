b2c5223c039dd8936b9bcb69047b1872fa478950
mrtwizard <b.m78990@gmail.com>
2018-12-14 12:49:06 +0100

    Add urls

--- includes/urls.php
+++ includes/urls.php
@@ -2,4 +2,8 @@
   $urls = array(
     'login' => '/zeblogphp/pages/login.php',
     'register' => '/zeblogphp/pages/register.php',
+    'logout' => '/zeblogphp/includes/logout.php',
+    'profile' => '/zeblogphp/account/profile.php',
+    'categories' => '/zeblogphp/pages/categories.php',
+    'articles' => '/zeblogphp/pages/articles.php',
   );
