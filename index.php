bc79d03f1634c5e4f817a858c1629030fe9c77f1
baptiste <b.m78990@gmail.com>
2019-01-07 16:20:51 +0100

    edit index file route

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-  $page = "Home";
-  require "./includes/header.php";
+  $page = "Homepage";
+  require __DIR__.'/includes/header.php';
+  require __DIR__.'/includes/navbar.php';
 ?>
-
 <div class="container">
-  
+  homepage
 </div>
-
 <?php
-  require "./includes/footer.php";
+  require __DIR__.'/includes/footer.php';
 ?>
