ec28e632bcfe72bbc212930c057cd46338b2d62c
baptiste <b.m78990@gmail.com>
2019-01-08 14:56:29 +0100

    Add footer component and style

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,9 @@
+  <footer>
+    <div>
+      <p>Copyright 2019 baptiste-mrt.fr</p>
+      <p><a href="https://baptiste-mrt.fr" target="_blank">Contact</a> <i class="fas fa-circle"></i> <a href="https://github.com/mrtwizard/zeblogphp" target="_blank">GitHub</a></p>
+    </div>
+  </footer>
   <script src="/zeblogphp/app.js"></script>
 </body>
 </html>
